23c9d8abd0cc00de57adf8db23c5336699f1bb61
Chunxiao Li <lcx@ynao.ac.cn>
2019-12-27 16:42:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     classifiers=[
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
         ],
     packages=setuptools.find_packages(),
